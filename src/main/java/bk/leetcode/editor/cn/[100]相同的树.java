4cd58c93ca16ebd28475fdf5aa835775a190cd8d
zhangtao <zhangtao@getui.com>
2021-01-12 23:17:05 +0800

    [feature]: Question 100 [https://leetcode-cn.com/problems/same-tree/].

---
+++ src/main/java/bk/leetcode/editor/cn/[100]相同的树.java
@@ -0,0 +1,91 @@
+//给定两个二叉树，编写一个函数来检验它们是否相同。 
+//
+// 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。 
+//
+// 示例 1: 
+//
+// 输入:       1         1
+//          / \       / \
+//         2   3     2   3
+//
+//        [1,2,3],   [1,2,3]
+//
+//输出: true 
+//
+// 示例 2: 
+//
+// 输入:      1          1
+//          /           \
+//         2             2
+//
+//        [1,2],     [1,null,2]
+//
+//输出: false
+// 
+//
+// 示例 3: 
+//
+// 输入:       1         1
+//          / \       / \
+//         2   1     1   2
+//
+//        [1,2,1],   [1,1,2]
+//
+//输出: false
+// 
+// Related Topics 树 深度优先搜索 
+// 👍 546 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+public class TreeNode {
+    int      val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null && q != null)
+            return false;
+        if (p != null && q == null)
+            return false;
+        if (p.val != q.val)
+            return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
