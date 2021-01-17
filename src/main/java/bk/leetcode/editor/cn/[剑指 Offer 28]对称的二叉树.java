9ae1d8eda094e01e0d918030cab93d570ad1ab44
zhangtao <zhangtao@getui.com>
2021-01-17 15:39:41 +0800

    [feature]: [剑指 Offer 28]对称的二叉树

---
+++ src/main/java/bk/leetcode/editor/cn/[剑指 Offer 28]对称的二叉树.java
@@ -0,0 +1,70 @@
+//请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。 
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。 
+//
+// 1 
+// / \ 
+// 2 2 
+// / \ / \ 
+//3 4 4 3 
+//但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的: 
+//
+// 1 
+// / \ 
+// 2 2 
+// \ \ 
+// 3 3 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：root = [1,2,2,3,4,4,3]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 输入：root = [1,2,2,null,3,null,3]
+//输出：false 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 1000 
+//
+// 注意：本题与主站 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/ 
+// Related Topics 树 
+// 👍 119 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+
+        return dfs(root.left, root.right);
+    }
+
+    boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null)
+            return true;
+
+        if (left == null || right == null)
+            return false;
+
+        return left.val == right.val && dfs(left.right, right.left) && dfs(left.left, right.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
