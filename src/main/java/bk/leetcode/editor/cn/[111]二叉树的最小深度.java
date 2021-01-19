ac7527aafc7aea6e82b493dfa8feb654f8966d0c
zhangtao <zhangtao@getui.com>
2021-01-19 20:42:02 +0800

    [feature]:[111]二叉树的最小深度

---
+++ src/main/java/bk/leetcode/editor/cn/[111]二叉树的最小深度.java
@@ -0,0 +1,99 @@
+//给定一个二叉树，找出其最小深度。 
+//
+// 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。 
+//
+// 说明：叶子节点是指没有子节点的节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [2,null,3,null,4,null,5,null,6]
+//输出：5
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数的范围在 [0, 105] 内 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 
+// 👍 426 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    int ans = 0;
+
+    public int minDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        return dfs(root);
+    }
+
+    int dfs(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        if (root.left == null && root.right == null) return;
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        if(Math.min(left,right)==1){
+            ans = Math.max(ans,Math.max(left,right));
+        }else {
+            ans = 
+        }
+
+        return Math.max(left,right) + 1;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
