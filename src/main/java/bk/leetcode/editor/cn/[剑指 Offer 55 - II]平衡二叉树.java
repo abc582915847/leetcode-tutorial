55bd092135598ccd29994395c4e5b8381c880576
zhangtao <zhangtao@getui.com>
2021-01-19 20:51:00 +0800

    [feature]:[剑指 Offer 55 - II]平衡二叉树

---
+++ src/main/java/bk/leetcode/editor/cn/[剑指 Offer 55 - II]平衡二叉树.java
@@ -0,0 +1,81 @@
+//输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。 
+//
+// 
+//
+// 示例 1: 
+//
+// 给定二叉树 [3,9,20,null,null,15,7] 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回 true 。 
+// 
+//示例 2: 
+//
+// 给定二叉树 [1,2,2,3,3,null,null,4,4] 
+//
+//        1
+//      / \
+//     2   2
+//    / \
+//   3   3
+//  / \
+// 4   4
+// 
+//
+// 返回 false 。 
+//
+// 
+//
+// 限制： 
+//
+// 
+// 1 <= 树的结点个数 <= 10000 
+// 
+//
+// 注意：本题与主站 110 题相同：https://leetcode-cn.com/problems/balanced-binary-tree/ 
+//
+// 
+// Related Topics 树 深度优先搜索 
+// 👍 103 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    boolean ans = true;
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+
+        dfs(root);
+        
+        return ans;
+    }
+
+    int dfs(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        if (Math.abs(left - right) > 1) {
+            ans = false;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
