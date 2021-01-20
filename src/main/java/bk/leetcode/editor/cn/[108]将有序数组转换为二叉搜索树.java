07c7664474a6a89aa5da6944add36131161a733e
zhangtao <zhangtao@getui.com>
2021-01-20 14:52:58 +0800

    [feature]:[108]将有序数组转换为二叉搜索树

---
+++ src/main/java/bk/leetcode/editor/cn/[108]将有序数组转换为二叉搜索树.java
@@ -0,0 +1,63 @@
+//将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。 
+//
+// 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。 
+//
+// 示例: 
+//
+// 给定有序数组: [-10,-3,0,5,9],
+//
+//一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+//
+//      0
+//     / \
+//   -3   9
+//   /   /
+// -10  5
+// 
+// Related Topics 树 深度优先搜索 
+// 👍 676 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.Arrays;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class TreeNode {
+    int      val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length - 1);
+    }
+
+    TreeNode dfs(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int mid = (left + right) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dfs(nums, left, mid - 1);
+        root.right = dfs(nums, mid + 1, right);
+        return root;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
